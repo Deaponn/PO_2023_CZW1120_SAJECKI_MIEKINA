ba0bcd82238107502b790bc489e187d46d6b4584
szym-mie <szym.mie@gmail.com>
2023-12-25 21:40:40 +0100

    Add passing state to the Launcher

--- src/main/java/agh/ics/oop/window/controller/Launcher.java
+++ src/main/java/agh/ics/oop/window/controller/Launcher.java
@@ -1,5 +1,10 @@
 package agh.ics.oop.window.controller;
 
+import agh.ics.oop.Configuration;
+import agh.ics.oop.model.EquatorialWorldMap;
+import agh.ics.oop.model.MapType;
+import agh.ics.oop.model.WorldMap;
+import agh.ics.oop.render.ImageMap;
 import agh.ics.oop.window.LayoutPath;
 import agh.ics.oop.window.Window;
 import agh.ics.oop.window.WindowController;
@@ -15,7 +20,12 @@ public class Launcher extends WindowController {
                 "Viewer",
                 LayoutPath.VIEWER.path
         );
-        viewerWindow.send("world_map", null);
+        Configuration configuration = new Configuration(13, 8, MapType.STANDARD, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.2f, 1, false);
+        WorldMap worldMap = new EquatorialWorldMap(configuration);
+        ImageMap imageMap = new ImageMap("resources/gfx", "png");
+
+        viewerWindow.send("world_map", worldMap);
+        viewerWindow.send("image_map", imageMap);
         this.viewerWindowList.add(viewerWindow);
         viewerWindow.show();
     }
