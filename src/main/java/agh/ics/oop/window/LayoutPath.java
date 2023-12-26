330bb940150eb72c3fee2a9757c105d7732b079d
szym-mie <szym.mie@gmail.com>
2023-12-26 17:18:44 +0100

    Update layout paths

--- src/main/java/agh/ics/oop/window/LayoutPath.java
+++ src/main/java/agh/ics/oop/window/LayoutPath.java
@@ -1,7 +1,7 @@
 package agh.ics.oop.window;
 
 public enum LayoutPath {
-    LAUCHER("fxml/launcher.fxml"),
+    LAUNCHER("fxml/launcher.fxml"),
     VIEWER("fxml/viewer.fxml");
 
     public final String path;
